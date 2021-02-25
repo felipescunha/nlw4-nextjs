52a2592983b30c98b648110ccc3762d5e01de96f
felipescunha <fcunha.ti@gmail.com>
2021-02-24 23:58:57 -0300

    componente challengeBox finalizado

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,46 @@
+import styles from '../styles/components/ChallengeBox.module.css';
+
+export function ChallengeBox() {
+  const hasActiveChallenge = true;
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      { hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400px</header>
+
+          <main>
+            <img src="icons/body.svg" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos.</p>
+          </main>
+
+          <footer>
+            <button
+              type="button"
+              className={styles.challengeFailedButton}
+              onAuxClick={() => {}}
+            >
+              Falhei
+            </button>
+            <button
+              type="button"
+              className={styles.challengeSucceededButton}
+              onAuxClick={() => {}}
+            >
+              Completei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo par receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up"/>
+            Avance de level completando desafios.
+          </p>
+        </div>
+      )}
+    </div>
+  )
+} 
