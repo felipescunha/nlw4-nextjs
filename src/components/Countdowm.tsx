c4d06b5e4b2cc6c5ad35c59c8bdd1accff12f2a5
felipescunha <fcunha.ti@gmail.com>
2021-02-26 12:03:39 -0300

    Adicionadas funcoes de context

--- src/components/Countdowm.tsx
+++ src/components/Countdowm.tsx
@@ -2,8 +2,6 @@ import { useState, useEffect, useContext } from 'react';
 import { CountdownContext } from '../contexts/CountdownContext';
 import styles from '../styles/components/Countdown.module.css';
 
-
-
 export function Countdown(){
 
   const {
@@ -15,11 +13,8 @@ export function Countdown(){
     resetCountdown
   } = useContext(CountdownContext)
   
-
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
-
-  
 
   return (
     <div>
@@ -63,8 +58,6 @@ export function Countdown(){
       }
         </>
       )}
-
-
     </div>
   );
 }
